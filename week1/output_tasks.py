19d6830db49a1442d368c335b9c61313fbc4c0d7
0nisad85 <0nisad85@solent.ac.uk>
2024-09-26 14:50:29 +0100

    Added code to display a simple message."
    Updated code to display a
    longer multi-line message.

--- week1/output_tasks.py
+++ week1/output_tasks.py
@@ -1,3 +1,10 @@
 # Display message to the screen
 print("Welcome to COM411!")
 #Added code to display a simple message#
+print("In week 1 we will learn...")
+print("")
+print("...How to use Git and Github")
+print("...how to output to the screen")
+print("...How to get user input")
+print("")
+print("I hope you are enjoying the lesson thus far!")
