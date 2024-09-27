6bf34b5cce03bf86286718767be41877f5136de9
0nisad85 <0nisad85@solent.ac.uk>
2024-09-27 16:14:40 +0100

    Added code to display a multi-line message
    Updated code to display a
    longer multi-line message.
    
    "Updated code to display a message with
    escape characters.
    
    Added code to display an
    ASCII art robot
    
    Added code to read user input."
    
    "Added code to display an ASCII robot
    with dynamic eyes.".

--- week1/input_tasks.py
+++ week1/input_tasks.py
@@ -1,5 +1,15 @@
 # Ask user to enter their name
-name = input("What is your name?")
-print(f"Nice to meet you {name}.")
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}.")
+eye = input("Please enter a character for the robots eye.")
+print("The robot's expression is now as follows:")
+print("######################")
+print(f"#  ({eye})   ({eye})         #")
+print("#                     #")
+print("#                     #")
+print("#                     #")
+print("#   ______________    #")
+print("######################")
+
 
 
