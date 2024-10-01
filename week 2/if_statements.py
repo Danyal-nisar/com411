fd7506e5fc46e703fef0236ac683d6c9e2939607
Danyal <0nisad85@solent.ac.uk>
2024-10-01 11:13:33 +0100

    ## Added code to demonstrate the use of an if...else statement.

--- week 2/if_statements.py
+++ week 2/if_statements.py
@@ -5,4 +5,13 @@ if book == "adventure":
 
 print("Finished reading book!")
 
+print("")
+#use input function to allow activity to be saved
+activity = input("Please enter the activity to be performed: ")
+#if function is used to tell system that if activity is calculate then print performing calculate or else something else
+if activity == "calculate":
+    print("Performing calculations...")
+else:
+    print("Performing the activity...")
 
+print("Activity completed!")
