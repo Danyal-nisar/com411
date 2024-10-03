0b2f0f4212e25ad2ebbf0c87d54918c7ef2378bd
0nisad85 <0nisad85@solent.ac.uk>
2024-10-03 15:44:39 +0100

    "Added code to demonstrate the use of an if...elif...else statement
    Added code to demonstrate the use of a modulus operator.
    
    "Added code to demonstrate the use of comparison
    operators
    
    "Added code to demonstrate how a variable can be used as
    a counter.
    
    Added code to demonstrate the use of a nested decision
    statement.

--- week 2/nested_if.py
+++ week 2/nested_if.py
@@ -1,5 +1,12 @@
-covertype =input("enter the type of cover of the book")
-if covertype == soft:
-    input("is the book perfect bound")
-    if input == yes:
+covertype = input("Enter the type of cover of the book : ")
+
+if covertype == "soft":
+    perfect_bound = input("Is the book perfect bound? : ")
+    if perfect_bound == "yes":
         print("Soft cover, perfect bound books are very popular!")
+    else:
+        print("Soft cover, not perfect bound books can still be very appealing!")
+elif covertype == "hard":
+    print("Books with hard covers can be more expensive!")
+
+
