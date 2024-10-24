c8e3aa4dbc6163fb50ee52da6251b4599003a073
Danyal <0nisad85@solent.ac.uk>
2024-10-24 22:14:47 +0100

    ## Added code to demonstrate the use of an if...else statement.
    Added code to demonstrate the use of multiple nested
    statements.
    
    Added code to demonstrate the use of a logical or
    operator.".
    
    Added code to demonstrate the use of a logical and
    operator.
    
    Added code to demonstrate the use of a while loop with
    ASCII art
    
    "Added code to demonstrate counting down with a for loop.".
    Added code to
    
    demonstrate the use of a while loop with
    len function."
    
    "Added code to demonstrate the use of summing with a
    while loop
    
    "Added code to demonstrate the use of a while loop in
    performing calculations with user input.".
    
    Added code to demonstrate the use of a simple for loop
    
    "Added code to demonstrate the use of for loops in string
    indexing.".
    
    "Added code to demonstrate how to reverse a string using a
    for loop."
    
    "Added code to demonstrate the use of membership
    operators with a for loop
    
    "Added code to demonstrate the use of one loop nested is
    another."
    
    Added code to demonstrate the use of nested loops and
    decisions."
    
    Added code to demonstrate the use of built-in functions:
    len, ord
    
    Added code to demonstrate the use of built-in functions:
    abs, range, chr

--- week 4/built_in_functions.py
+++ week 4/built_in_functions.py
@@ -10,3 +10,23 @@ print("Program Started!")
 get_ascii_code() #function being called
 
 print("Program Ended!")
+
+print("")
+
+
+def ascii_program():
+    print("Program Started!")
+
+    ascii_code = abs(int(input("Please enter an ASCII code: ")))
+
+    if ascii_code in range(32, 127):
+        print(f"The character represented by the ASCII code {ascii_code} is: {chr(ascii_code)}")
+    else:
+        print("Error: The entered code is not within the printable ASCII range (32-126).")
+
+    print("Program Ended!")
+
+ascii_program()
+
+print("")
+
