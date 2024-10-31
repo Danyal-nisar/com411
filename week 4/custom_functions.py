8656f7056fba74919964d0f80da7f74c8dee911b
0nisad85 <0nisad85@solent.ac.uk>
2024-10-31 13:08:06 +0000

    Added code to demonstrate the use of a user-defined function

---
+++ week 4/custom_functions.py
@@ -0,0 +1,11 @@
+def listen():
+    # Ask the user to enter a word representing a sound
+    sound = input("What sound did you hear?\n")
+    # Display the message with the entered sound
+    print(f"That was a loud {sound}!")
+
+# Call the listen function
+listen()
+
+print("")
+
