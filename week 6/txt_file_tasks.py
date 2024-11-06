fcdd857fa6f96a21a05bb4194f6cf58e188f72cb
Danyal <0nisad85@solent.ac.uk>
2024-11-06 18:18:57 +0000

    Added code to demonstrate how to read a text file

--- week 6/txt_file_tasks.py
+++ week 6/txt_file_tasks.py
@@ -13,3 +13,33 @@ def run():
 
 run()
 
+print("")
+
+
+def display_chars(file_path, num_chars):
+    with open(file_path, 'r') as file:
+        text = file.read(num_chars)
+        print("The first", num_chars, "characters are:")
+        print(text)
+
+def display_line(file_path):
+    with open(file_path, 'r') as file:
+        line = file.readline()
+        print("The first line is:")
+        print(line)
+
+def display_text(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+        print("The full text is:")
+        print(text)
+
+def run_task2():
+    display_chars("library.txt", 5)
+    display_line("library.txt")
+    display_text("library.txt")
+
+if __name__ == "__main__":
+    run_task2()
+
+
