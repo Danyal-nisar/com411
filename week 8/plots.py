0df77052ece6deb3f98e39f874aebbcf068d00c2
0nisad85 <0nisad85@solent.ac.uk>
2024-11-21 11:13:54 +0000

    .

--- week 8/plots.py
+++ week 8/plots.py
@@ -1,17 +1,21 @@
+# plots.py
 import matplotlib.pyplot as plt
 
 def display_line(x_values, y_values):
-    plt.plot(x_values, y_values)
-    plt.xlabel("X Values")
-    plt.ylabel("Y Values")
-    plt.title("Line Plot of x vs. y")
+    plt.plot(x_values, y_values, label='y = x^2')
+    plt.title("Basic Line Plot")
+    plt.xlabel("x values")
+    plt.ylabel("y values")
     plt.grid(True)
+    plt.legend()
     plt.show()
 
 def run_task1():
-
     x_values = [1, 2, 3, 4, 5]
     y_values = [1, 4, 9, 16, 25]
-
     display_line(x_values, y_values)
+
+if __name__ == "__main__":
+    run_task1()
+
 
